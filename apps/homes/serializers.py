a2ad68901998e393e4fddee44b41ea9bf33e3dcf
Dilmurod <eshmamatovdilmurod7@gmail.com>
2025-10-11 14:56:05 +0500

    color serializers

--- apps/homes/serializers.py
+++ apps/homes/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from apps.homes.models import Category, Size, HomeImage, Home
+from apps.homes.models import Category, Size, HomeImage, Home, Color
 
 
 class CategorySerializers(serializers.ModelSerializer):
@@ -28,11 +28,21 @@ class HomeImageSerializers(serializers.ModelSerializer):
         )
 
 
+class ColorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Color
+        fields = (
+            "id", "name"
+        )
+
+
 class HomeSerializers(serializers.ModelSerializer):
-    size = SizeSerializers(read_only=True)
+    size = SizeSerializers(read_only=True, many=True)
     category = CategorySerializers(read_only=True)
     images = HomeImageSerializers(read_only=True, many=True)
     discount_price = serializers.SerializerMethodField(read_only=True)
+    color = ColorSerializer(many=True)
 
     class Meta:
         model = Home
